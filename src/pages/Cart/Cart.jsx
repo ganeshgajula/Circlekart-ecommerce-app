f1cf313ec8e04aedd4ad938dc1ccc4bd5b315b7c
ganesh <ganeshgajula22@gmail.com>
2021-04-18 08:39:35 +0530

    feat: major cart functionalities

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -16,31 +16,49 @@ export const Cart = () => {
           inStock,
           fastDelivery,
           quantity,
-        }) => (
-          <div
-            key={id}
-            style={{
-              border: "1px solid #4B5563",
-              borderRadius: "0 0 0.5rem 0.5rem",
-              margin: "10rem 1rem 1rem",
-              maxWidth: "20%",
-              padding: "0 0rem 1rem",
-            }}
-          >
-            <img src={image} width="100%" height="auto" alt={productName} />
-            <h3> {name} </h3>
-            <div>Rs. {price}</div>
-            {inStock && <div> In Stock </div>}
-            {!inStock && <div> Out of Stock </div>}
+        }) =>
+          quantity === 0 ? null : (
+            <div
+              key={id}
+              style={{
+                border: "1px solid #4B5563",
+                borderRadius: "0 0 0.5rem 0.5rem",
+                margin: "10rem 1rem 1rem",
+                maxWidth: "20%",
+                padding: "0 0rem 1rem",
+              }}
+            >
+              <img src={image} width="100%" height="auto" alt={productName} />
+              <h3> {name} </h3>
+              <div>Rs. {price}</div>
+              {inStock && <div> In Stock </div>}
+              {!inStock && <div> Out of Stock </div>}
 
-            {fastDelivery ? (
-              <div> Fast Delivery </div>
-            ) : (
-              <div> 3 days minimum </div>
-            )}
-            <div>{quantity}</div>
-          </div>
-        )
+              {fastDelivery ? (
+                <div> Fast Delivery </div>
+              ) : (
+                <div> 3 days minimum </div>
+              )}
+              <button
+                onClick={() => dataDispatch({ type: "DECREMENT", payload: id })}
+              >
+                -
+              </button>
+              <span>{quantity}</span>
+              <button
+                onClick={() => dataDispatch({ type: "INCREMENT", payload: id })}
+              >
+                +
+              </button>
+              <button
+                onClick={() =>
+                  dataDispatch({ type: "REMOVE_FROM_CART", payload: id })
+                }
+              >
+                Remove
+              </button>
+            </div>
+          )
       )}
     </div>
   );
--- src/reducer/dataReducer.js
+++ src/reducer/dataReducer.js
@@ -8,6 +8,34 @@ export const dataReducer = (state, action) => {
     case "ADD_TO_CART":
       return { ...state, itemsInCart: [...state.itemsInCart, action.payload] };
 
+    case "INCREMENT":
+      return {
+        ...state,
+        itemsInCart: state.itemsInCart.map((cartItem) =>
+          cartItem.id === action.payload
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        ),
+      };
+
+    case "DECREMENT":
+      return {
+        ...state,
+        itemsInCart: state.itemsInCart.map((cartItem) =>
+          cartItem.id === action.payload
+            ? { ...cartItem, quantity: cartItem.quantity - 1 }
+            : cartItem
+        ),
+      };
+
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        itemsInCart: state.itemsInCart.filter(
+          (cartItem) => cartItem.id !== action.payload
+        ),
+      };
+
     default:
       return state;
   }
