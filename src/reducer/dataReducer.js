947d303f4035cb2ded1b246e2047bf894cf5ec23
ganesh <ganeshgajula22@gmail.com>
2021-05-04 12:56:07 +0530

    remove: cleaned  code

--- src/reducer/dataReducer.js
+++ src/reducer/dataReducer.js
@@ -42,16 +42,6 @@ export const dataReducer = (state, action) => {
         itemsInWishlist: [...state.itemsInWishlist, action.payload],
       };
 
-    case "ADD_TO_WISHLIST_AND_CHECK_FOR_DUPLICATION":
-      return {
-        ...state,
-        itemsInWishlist: state.itemsInWishlist.map(({ _id }) =>
-          _id !== action.payload._id
-            ? [...state.itemsInWishlist, action.payload]
-            : null
-        ),
-      };
-
     case "REMOVE_FROM_WISHLIST":
       return {
         ...state,
