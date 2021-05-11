9b076b11a3e1b31abba5cefa183bdfaa199bae5d
ganesh <ganeshgajula22@gmail.com>
2021-05-11 19:58:12 +0530

    feat: created private routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,19 @@ import "./App.css";
 import { Home } from "./pages/Home/Home";
 import { Wishlist } from "./pages/Wishlist/Wishlist";
 import { Cart } from "./pages/Cart/Cart";
+import { Login } from "./pages/Login/Login";
 import { ProductListing } from "./pages/ProductListing/ProductListing";
 import { ProductDetail } from "./pages/ProductDetail/ProductDetail";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useProducts } from "./context/ProductsProvider";
 import axios from "axios";
 
+import { useAuth } from "./context/AuthProvider";
+
 const App = () => {
   const { productsDispatch } = useProducts();
+
+  const { isUserLoggedIn } = useAuth();
 
   useEffect(() => {
     (async () => {
@@ -26,13 +31,22 @@ const App = () => {
     })();
   }, []);
 
+  function PrivateRoute({ path, ...props }) {
+    return isUserLoggedIn ? (
+      <Route {...props} path={path} />
+    ) : (
+      <Navigate state={{ from: path }} replace to="/login" />
+    );
+  }
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/products" element={<ProductListing />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/cart" element={<Cart />} />
+        <PrivateRoute path="/wishlist" element={<Wishlist />} />
+        <PrivateRoute path="/cart" element={<Cart />} />
         <Route path="/productDetail" element={<ProductDetail />} />
       </Routes>
     </div>
