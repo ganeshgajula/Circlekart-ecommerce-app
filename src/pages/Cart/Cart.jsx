c600bdd8451fde70cc598bb9052da35fb9766166
ganesh <ganeshgajula22@gmail.com>
2021-05-22 15:26:10 +0530

    refactor: added server connectivity and modified as per backend

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { useData } from "../../context/DataProvider";
-import "../Cart/Cart.css";
+import { useAuth } from "../../context/AuthProvider";
 import { DeleteSvg } from "../../components/Reusable-Svgs/svgs";
 import { isItemPresent } from "../../components/utils/utils";
 import { EmptyCart } from "../../components/EmptyCart/EmptyCart";
+import axios from "axios";
+import "../Cart/Cart.css";
 
 export const Cart = () => {
   const {
@@ -12,34 +14,68 @@ export const Cart = () => {
     dataDispatch,
   } = useData();
 
-  const getTotal = (cartItems) =>
-    cartItems.reduce((total, value) => total + value.price * value.quantity, 0);
+  const { userId } = useAuth();
 
-  const addToWishlistAndRemoveFromCart = (
-    _id,
-    name,
-    image,
-    price,
-    productName,
-    inStock,
-    fastDelivery
-  ) => {
-    dataDispatch({
-      type: "ADD_TO_WISHLIST",
-      payload: {
+  const getTotal = (cartItems) =>
+    cartItems.reduce(
+      (total, value) => total + value.productId.price * value.quantity,
+      0
+    );
+
+  const incrementQuantity = async (_id, quantity) => {
+    const {
+      data: { cart },
+      status,
+    } = await axios.post(
+      `https://api-circlekart.herokuapp.com/carts/${userId}/cart`,
+      {
         _id,
-        name,
-        image,
-        price,
-        productName,
-        inStock,
-        fastDelivery,
-      },
-    });
-    dataDispatch({
-      type: "REMOVE_FROM_CART",
-      payload: _id,
-    });
+        quantity: quantity + 1,
+        isActive: true,
+      }
+    );
+    if (status === 200) {
+      dataDispatch({ type: "LOAD_CART", payload: cart });
+    }
+  };
+
+  const decrementQuantity = async (_id, quantity) => {
+    const {
+      data: { cart },
+      status,
+    } = await axios.post(
+      `https://api-circlekart.herokuapp.com/carts/${userId}/cart`,
+      { _id, quantity: quantity - 1, isActive: true }
+    );
+    if (status === 200) {
+      dataDispatch({ type: "LOAD_CART", payload: cart });
+    }
+  };
+
+  const removeProductFromCart = async (_id) => {
+    const {
+      data: { cart },
+      status,
+    } = await axios.post(
+      `https://api-circlekart.herokuapp.com/carts/${userId}/cart`,
+      { _id, quantity: 0, isActive: false }
+    );
+    if (status === 200) {
+      dataDispatch({ type: "LOAD_CART", payload: cart });
+    }
+  };
+
+  const addProductToWishlist = async (_id) => {
+    const {
+      data: { wishlist },
+      status,
+    } = await axios.post(
+      `https://api-circlekart.herokuapp.com/wishlists/${userId}/wishlist`,
+      { _id, isActive: true }
+    );
+    if (status === 200) {
+      dataDispatch({ type: "LOAD_WISHLIST", payload: wishlist });
+    }
   };
 
   return (
@@ -56,129 +92,95 @@ export const Cart = () => {
                 Total: Rs.{getTotal(itemsInCart)}
               </span>
             </div>
-            {itemsInCart.map(
-              ({
-                _id,
-                name,
-                image,
-                price,
-                productName,
-                inStock,
-                fastDelivery,
-                quantity,
-              }) =>
-                quantity === 0 ? null : (
-                  <div
-                    key={_id}
-                    style={{
-                      border: "1px solid #e5e7eb",
-                      borderRadius: "0.5rem",
-                      margin: "1rem",
-                      padding: "0.9rem",
-                    }}
-                  >
-                    <div className="cartItem">
-                      <img
-                        src={image}
-                        alt={productName}
-                        className="cartItem-img"
-                      />
+            {itemsInCart.map(({ productId, quantity, isActive }) =>
+              quantity === 0 ? null : (
+                <div
+                  key={productId._id}
+                  style={{
+                    border: "1px solid #e5e7eb",
+                    borderRadius: "0.5rem",
+                    margin: "1rem",
+                    padding: "0.9rem",
+                  }}
+                >
+                  <div className="cartItem">
+                    <img
+                      src={productId.image}
+                      alt="product"
+                      className="cartItem-img"
+                    />
 
-                      <span className="cartItem-description">
-                        <div className="product-info">
-                          <h1 className="product-name"> {name} </h1>
-                          <p className="product-price">Rs. {price}</p>
-                        </div>
-                        <div className="product-availability-info">
-                          {inStock && (
-                            <div className="stock-status"> In Stock </div>
-                          )}
-                          {!inStock && (
-                            <div className="stock-status"> Out of Stock </div>
-                          )}
-                          {fastDelivery ? (
-                            <div className="delivery-status">
-                              {" "}
-                              Fast Delivery{" "}
-                            </div>
-                          ) : (
-                            <div className="delivery-status">
-                              {" "}
-                              3 days minimum{" "}
-                            </div>
-                          )}
-                        </div>
-                        <div className="cart-btns">
-                          <button
-                            onClick={() =>
-                              quantity < 2
-                                ? dataDispatch({
-                                    type: "REMOVE_FROM_CART",
-                                    payload: _id,
-                                  })
-                                : dataDispatch({
-                                    type: "DECREMENT",
-                                    payload: _id,
-                                  })
-                            }
-                            className="btn-outline btn-sm count-btn"
-                          >
-                            {quantity < 2 ? <DeleteSvg /> : "-"}
-                          </button>
-
-                          <span className="quantity-count">{quantity}</span>
-
-                          <button
-                            onClick={() =>
-                              dataDispatch({ type: "INCREMENT", payload: _id })
-                            }
-                            className="btn-outline btn-sm count-btn"
-                          >
-                            +
-                          </button>
-                        </div>
-                      </span>
-                    </div>
-
-                    <div className="action-btns">
-                      {quantity > 1 ? (
+                    <span className="cartItem-description">
+                      <div className="product-info">
+                        <h1 className="product-name"> {productId.name} </h1>
+                        <p className="product-price">Rs. {productId.price}</p>
+                      </div>
+                      <div className="product-availability-info">
+                        {productId.inStock && (
+                          <div className="stock-status"> In Stock </div>
+                        )}
+                        {!productId.inStock && (
+                          <div className="stock-status"> Out of Stock </div>
+                        )}
+                        {productId.fastDelivery ? (
+                          <div className="delivery-status"> Fast Delivery </div>
+                        ) : (
+                          <div className="delivery-status">
+                            {" "}
+                            3 days minimum{" "}
+                          </div>
+                        )}
+                      </div>
+                      <div className="cart-btns">
                         <button
-                          className="btn-outline btn-sm"
                           onClick={() =>
-                            dataDispatch({
-                              type: "REMOVE_FROM_CART",
-                              payload: _id,
-                            })
+                            quantity < 2
+                              ? removeProductFromCart(productId._id)
+                              : decrementQuantity(productId._id, quantity)
                           }
+                          className="btn-outline btn-sm count-btn"
                         >
-                          Remove
+                          {quantity < 2 ? <DeleteSvg /> : "-"}
                         </button>
-                      ) : null}
 
+                        <span className="quantity-count">{quantity}</span>
+
+                        <button
+                          onClick={() =>
+                            incrementQuantity(productId._id, quantity)
+                          }
+                          className="btn-outline btn-sm count-btn"
+                        >
+                          +
+                        </button>
+                      </div>
+                    </span>
+                  </div>
+
+                  <div className="action-btns">
+                    {quantity > 1 ? (
                       <button
                         className="btn-outline btn-sm"
-                        onClick={() =>
-                          !isItemPresent(itemsInWishlist, _id)
-                            ? addToWishlistAndRemoveFromCart(
-                                _id,
-                                name,
-                                image,
-                                price,
-                                productName,
-                                inStock,
-                                fastDelivery
-                              )
-                            : dataDispatch({
-                                type: "REMOVE_FROM_CART",
-                                payload: _id,
-                              })
-                        }
+                        onClick={() => removeProductFromCart(productId._id)}
                       >
-                        Move to wishlist
+                        Remove
                       </button>
-                    </div>
+                    ) : null}
+
+                    <button
+                      className="btn-outline btn-sm"
+                      onClick={() =>
+                        !isItemPresent(itemsInWishlist, productId._id)
+                          ? addProductToWishlist(productId._id) &&
+                            removeProductFromCart(productId._id)
+                          : removeProductFromCart(productId._id)
+                      }
+                    >
+                      Move to wishlist
+                    </button>
                   </div>
-                )
+                </div>
+              )
             )}
           </div>
           <aside>
