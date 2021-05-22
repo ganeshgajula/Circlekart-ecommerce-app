40e27720972a9dbb44f5c6b8695fc0b817937b8a
ganesh <ganeshgajula22@gmail.com>
2021-05-22 15:10:09 +0530

    refactor: changed as per backend

--- src/reducer/dataReducer.js
+++ src/reducer/dataReducer.js
@@ -5,60 +5,11 @@ export const data = {
 
 export const dataReducer = (state, action) => {
   switch (action.type) {
-    case "ADD_TO_CART":
-      return { ...state, itemsInCart: [...state.itemsInCart, action.payload] };
+    case "LOAD_CART":
+      return { ...state, itemsInCart: action.payload };
 
-    case "INCREMENT":
-      return {
-        ...state,
-        itemsInCart: state.itemsInCart.map((cartItem) =>
-          cartItem._id === action.payload
-            ? { ...cartItem, quantity: cartItem.quantity + 1 }
-            : cartItem
-        ),
-      };
-
-    case "DECREMENT":
-      return {
-        ...state,
-        itemsInCart: state.itemsInCart.map((cartItem) =>
-          cartItem._id === action.payload
-            ? { ...cartItem, quantity: cartItem.quantity - 1 }
-            : cartItem
-        ),
-      };
-
-    case "REMOVE_FROM_CART":
-      return {
-        ...state,
-        itemsInCart: state.itemsInCart.filter(
-          (cartItem) => cartItem._id !== action.payload
-        ),
-      };
-
-    case "ADD_TO_WISHLIST":
-      return {
-        ...state,
-        itemsInWishlist: [...state.itemsInWishlist, action.payload],
-      };
-
-    case "REMOVE_FROM_WISHLIST":
-      return {
-        ...state,
-        itemsInWishlist: state.itemsInWishlist.filter(
-          (wishlistedItem) => wishlistedItem._id !== action.payload
-        ),
-      };
-
-    case "ADD_ITEM_WITH_INCREASED_QUANTITY":
-      return {
-        ...state,
-        itemsInCart: state.itemsInCart.map((cartItem) =>
-          cartItem._id === action.payload
-            ? { ...cartItem, quantity: cartItem.quantity + 1 }
-            : cartItem
-        ),
-      };
+    case "LOAD_WISHLIST":
+      return { ...state, itemsInWishlist: action.payload };
 
     default:
       return state;
