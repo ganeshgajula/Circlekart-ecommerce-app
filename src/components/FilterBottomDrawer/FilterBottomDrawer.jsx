eb88c89d2378d48f23ab84a76d75b1a4548d9f56
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-09-27 14:22:46 +0530

    style: bottom drawers

--- src/components/FilterBottomDrawer/FilterBottomDrawer.jsx
+++ src/components/FilterBottomDrawer/FilterBottomDrawer.jsx
@@ -22,35 +22,37 @@ export const FilterBottomDrawer = ({ setShowFilterDrawer }) => {
   };
 
   return (
-    <div className="bottom-drawer">
-      <div className="drawer-header">
-        <span className="drawer-title">Filter By</span>
-        <span onClick={() => setShowFilterDrawer(false)}>
-          <CloseIcon />
-        </span>
-      </div>
-      <div className="options">
-        <label className="form-label">
-          Include Out of Stock
-          <input
-            type="checkbox"
-            onChange={() => setToggleStock((prev) => !prev)}
-            checked={toggleStock}
-          />
-        </label>
-        <label className="form-label">
-          Fast Delivery Only
-          <input
-            type="checkbox"
-            onChange={() => setToggleDelivery((prev) => !prev)}
-            checked={toggleDelivery}
-          />
-        </label>
-      </div>
-      <div className="filter-btn">
-        <button className="btn-apply" onClick={applyFilters}>
-          Apply
-        </button>
+    <div className="outer-area">
+      <div className="bottom-drawer">
+        <div className="drawer-header">
+          <span className="drawer-title">Filter By</span>
+          <span onClick={() => setShowFilterDrawer(false)}>
+            <CloseIcon />
+          </span>
+        </div>
+        <div className="options">
+          <label className="form-label">
+            Include Out of Stock
+            <input
+              type="checkbox"
+              onChange={() => setToggleStock((prev) => !prev)}
+              checked={toggleStock}
+            />
+          </label>
+          <label className="form-label">
+            Fast Delivery Only
+            <input
+              type="checkbox"
+              onChange={() => setToggleDelivery((prev) => !prev)}
+              checked={toggleDelivery}
+            />
+          </label>
+        </div>
+        <div className="filter-btn">
+          <button className="btn-apply" onClick={applyFilters}>
+            Apply
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/SortBottomDrawer/SortBottomDrawer.jsx
+++ src/components/SortBottomDrawer/SortBottomDrawer.jsx
@@ -10,42 +10,46 @@ export const SortBottomDrawer = ({ setShowSortDrawer }) => {
   } = useProducts();
 
   return (
-    <div className="bottom-drawer">
-      <div className="drawer-header">
-        <span className="drawer-title">SORT BY</span>
-        <span onClick={() => setShowSortDrawer(false)}>
-          <CloseIcon />
-        </span>
-      </div>
-      <div className="options">
-        <label className="form-label" onClick={() => setShowSortDrawer(false)}>
-          Price - High to Low
-          <input
-            type="radio"
-            name="sort"
-            onChange={() =>
-              productsDispatch({
-                type: "SORT",
-                payload: "PRICE_HIGH_TO_LOW",
-              })
-            }
-            checked={sortBy && sortBy === "PRICE_HIGH_TO_LOW"}
-          />
-        </label>
-        <label className="form-label" onClick={() => setShowSortDrawer(false)}>
-          Price - Low to High
-          <input
-            type="radio"
-            name="sort"
-            onChange={() =>
-              productsDispatch({
-                type: "SORT",
-                payload: "PRICE_LOW_TO_HIGH",
-              })
-            }
-            checked={sortBy && sortBy === "PRICE_LOW_TO_HIGH"}
-          />
-        </label>
+    <div className="outer-area">
+      <div className="bottom-drawer">
+        <div className="drawer-header">
+          <span className="drawer-title">SORT BY</span>
+          <span onClick={() => setShowSortDrawer(false)}>
+            <CloseIcon />
+          </span>
+        </div>
+        <div className="options">
+          <label className="form-label">
+            Price - High to Low
+            <input
+              type="radio"
+              name="sort"
+              onChange={() => {
+                productsDispatch({
+                  type: "SORT",
+                  payload: "PRICE_HIGH_TO_LOW",
+                });
+                setShowSortDrawer(false);
+              }}
+              checked={sortBy && sortBy === "PRICE_HIGH_TO_LOW"}
+            />
+          </label>
+          <label className="form-label">
+            Price - Low to High
+            <input
+              type="radio"
+              name="sort"
+              onChange={() => {
+                productsDispatch({
+                  type: "SORT",
+                  payload: "PRICE_LOW_TO_HIGH",
+                });
+                setShowSortDrawer(false);
+              }}
+              checked={sortBy && sortBy === "PRICE_LOW_TO_HIGH"}
+            />
+          </label>
+        </div>
       </div>
     </div>
   );
